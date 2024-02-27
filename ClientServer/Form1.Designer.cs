aa3288a951d381ff8723a28eb7a4a3af58f01da6
EnesKaleci <eneskaleci2@gmail.com>
2024-02-28 01:07:29 +0300

    ServerClient
    "It serves for communicating via messages on a shared network

---
+++ ClientServer/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ClientServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.txthost = new System.Windows.Forms.TextBox();
+            this.txtport = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(437, 88);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnconnect.TabIndex = 0;
+            this.btnconnect.Text = "Start";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // txthost
+            // 
+            this.txthost.Location = new System.Drawing.Point(164, 90);
+            this.txthost.Name = "txthost";
+            this.txthost.Size = new System.Drawing.Size(100, 20);
+            this.txthost.TabIndex = 1;
+            // 
+            // txtport
+            // 
+            this.txtport.Location = new System.Drawing.Point(315, 88);
+            this.txtport.Name = "txtport";
+            this.txtport.Size = new System.Drawing.Size(100, 20);
+            this.txtport.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(518, 88);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(129, 117);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(464, 165);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtport);
+            this.Controls.Add(this.txthost);
+            this.Controls.Add(this.btnconnect);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconnect;
+        private System.Windows.Forms.TextBox txthost;
+        private System.Windows.Forms.TextBox txtport;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ ClientServer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientServer
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpServer server;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+
+        }
+
+        private void btnconnect_Click(object sender, EventArgs e)
+        {
+            //Start server host
+            textBox3.Text += "Server starting...";
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txthost.Text);
+            server.Start(ip, Convert.ToInt32(txtport.Text));
+        }
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+           
+        }
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            //Update mesage to txtStatus
+            textBox3.Invoke((MethodInvoker)delegate ()
+            {
+                textBox3.Text += e.MessageString;
+                e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+            });
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+}
